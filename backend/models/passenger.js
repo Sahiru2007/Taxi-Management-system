eb8b64cf9df308cca9f05999d3dc3276549bd3e8
thimeth04 <158057572+thimeth04@users.noreply.github.com>
2024-01-29 19:31:24 +0530

    smsregistration

--- backend/models/passenger.js
+++ backend/models/passenger.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const Joi = require('joi'); 
+
 const passwordComplexity = require("joi-password-complexity" )
 
 const passengerSchema = new mongoose.Schema({
